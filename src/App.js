14916815a267f1b0daa07dd746eb52ddef1367a8
facudelucia <facundodeluciajudo@gmail.com>
2022-03-24 13:07:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import Counter from "./Counter";
+
+const CounterWrapper = styled.div`
+  margin: auto;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const Root = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <CounterWrapper>
+        <Counter />
+      </CounterWrapper>
+    </Wrapper>
   );
-}
+};
 
-export default App;
+export default Root;
